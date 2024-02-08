5b4853333018d26ced99067b521c2f4f5d9a32db
C-503 <yeffersonv503@gmail.com>
2024-02-08 13:01:25 -0600

    Se agrega la tarea

---
+++ src/main/java/clases/pacientes.java
@@ -0,0 +1,179 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author anaca
+ */
+public class pacientes {
+
+    /**
+     * @return the nopaciente
+     */
+    public String getNopaciente() {
+        return nopaciente;
+    }
+
+    /**
+     * @param nopaciente the nopaciente to set
+     */
+    public void setNopaciente(String nopaciente) {
+        this.nopaciente = nopaciente;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the dpi
+     */
+    public String getDpi() {
+        return dpi;
+    }
+
+    /**
+     * @param dpi the dpi to set
+     */
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    /**
+     * @return the lugar
+     */
+    public String getLugar() {
+        return lugar;
+    }
+
+    /**
+     * @param lugar the lugar to set
+     */
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    /**
+     * @return the alergias
+     */
+    public String getAlergias() {
+        return alergias;
+    }
+
+    /**
+     * @param alergias the alergias to set
+     */
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the horaingreso
+     */
+    public String getHoraingreso() {
+        return horaingreso;
+    }
+
+    /**
+     * @param horaingreso the horaingreso to set
+     */
+    public void setHoraingreso(String horaingreso) {
+        this.horaingreso = horaingreso;
+    }
+
+    /**
+     * @return the horaegreso
+     */
+    public String getHoraegreso() {
+        return horaegreso;
+    }
+
+    /**
+     * @param horaegreso the horaegreso to set
+     */
+    public void setHoraegreso(String horaegreso) {
+        this.horaegreso = horaegreso;
+    }
+    
+    
+     private String nopaciente;
+    private String nombre;
+    private String apellido;
+    private String sexo;
+     private String fecha;
+    private String dpi;
+    private String lugar;
+    private String alergias;
+    private String telefono;
+    private String horaingreso;
+    private String horaegreso;
+}
---
+++ src/main/java/clases/pacientes2.java
@@ -0,0 +1,179 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author anaca
+ */
+public class pacientes2 {
+
+    /**
+     * @return the nopaciente1
+     */
+    public String getNopaciente1() {
+        return nopaciente1;
+    }
+
+    /**
+     * @param nopaciente1 the nopaciente1 to set
+     */
+    public void setNopaciente1(String nopaciente1) {
+        this.nopaciente1 = nopaciente1;
+    }
+
+    /**
+     * @return the nombre1
+     */
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    /**
+     * @param nombre1 the nombre1 to set
+     */
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    /**
+     * @return the apellido1
+     */
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    /**
+     * @param apellido1 the apellido1 to set
+     */
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    /**
+     * @return the sexo1
+     */
+    public String getSexo1() {
+        return sexo1;
+    }
+
+    /**
+     * @param sexo1 the sexo1 to set
+     */
+    public void setSexo1(String sexo1) {
+        this.sexo1 = sexo1;
+    }
+
+    /**
+     * @return the fecha1
+     */
+    public String getFecha1() {
+        return fecha1;
+    }
+
+    /**
+     * @param fecha1 the fecha1 to set
+     */
+    public void setFecha1(String fecha1) {
+        this.fecha1 = fecha1;
+    }
+
+    /**
+     * @return the dpi1
+     */
+    public String getDpi1() {
+        return dpi1;
+    }
+
+    /**
+     * @param dpi1 the dpi1 to set
+     */
+    public void setDpi1(String dpi1) {
+        this.dpi1 = dpi1;
+    }
+
+    /**
+     * @return the lugar1
+     */
+    public String getLugar1() {
+        return lugar1;
+    }
+
+    /**
+     * @param lugar1 the lugar1 to set
+     */
+    public void setLugar1(String lugar1) {
+        this.lugar1 = lugar1;
+    }
+
+    /**
+     * @return the alergias1
+     */
+    public String getAlergias1() {
+        return alergias1;
+    }
+
+    /**
+     * @param alergias1 the alergias1 to set
+     */
+    public void setAlergias1(String alergias1) {
+        this.alergias1 = alergias1;
+    }
+
+    /**
+     * @return the telefono1
+     */
+    public String getTelefono1() {
+        return telefono1;
+    }
+
+    /**
+     * @param telefono1 the telefono1 to set
+     */
+    public void setTelefono1(String telefono1) {
+        this.telefono1 = telefono1;
+    }
+
+    /**
+     * @return the horaingreso1
+     */
+    public String getHoraingreso1() {
+        return horaingreso1;
+    }
+
+    /**
+     * @param horaingreso1 the horaingreso1 to set
+     */
+    public void setHoraingreso1(String horaingreso1) {
+        this.horaingreso1 = horaingreso1;
+    }
+
+    /**
+     * @return the horaegreso1
+     */
+    public String getHoraegreso1() {
+        return horaegreso1;
+    }
+
+    /**
+     * @param horaegreso1 the horaegreso1 to set
+     */
+    public void setHoraegreso1(String horaegreso1) {
+        this.horaegreso1 = horaegreso1;
+    }
+    
+    
+    private static String nopaciente1;
+    private static String nombre1;
+    private static String apellido1;
+    private static String sexo1;
+    private static String fecha1;
+    private static String dpi1;
+    private static String lugar1;
+    private static String alergias1;
+    private static String telefono1;
+    private static String horaingreso1;
+    private static String horaegreso1;
+}
